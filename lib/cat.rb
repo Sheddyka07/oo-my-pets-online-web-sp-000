f8fceb7eefb654d30d184ce98c38bfd9d63c8940
Kelly Sheddy <jungaka07@gmail.com>
2019-01-07 14:36:15 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,21 +1,8 @@
 class Cat
   attr_reader :name, :mood
   
-  def initialize(name, mood)  
+  def initialize(name)  
     @name = name
-    @mood = mood
-  end
- 
-  def mood
-    if playful?
-      puts "playful"
-    else
-      puts "nervous"
-    end
-  end
- 
-  def playful?
-    mood == 'playful'
   end
   
 end
