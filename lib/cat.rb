f38955ff46078145c5f8fa0472fb50b52e1aa2a1
Kelly Sheddy <jungaka07@gmail.com>
2019-01-07 14:34:47 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,20 @@
 class Cat
-  # code goes here
+  attr_accessor :name, :mood
+  
+  def initialize(name)  
+    @name = name
+  end
+ 
+  def mood
+    if playful?
+      puts "playful"
+    else
+      puts "nervous"
+    end
+  end
+ 
+  def playful?
+    mood == 'playful'
+  end
+  
 end
