e72fba7f5f0c02b61565b76225ac3f0c9e7dc781
Kelly Sheddy <jungaka07@gmail.com>
2019-01-07 14:40:45 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,9 +1,8 @@
 class Owner
   attr_reader :species
-  attr_accessor :mood
   
-  def initialize(species, mood = "nervous")  
-    @name = name
-    @mood = mood
+  
+  def initialize(species)  
+    @species = species
   end
 end
